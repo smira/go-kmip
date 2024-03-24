a60eb3ec6f3318e11e6dc302b7f6e65f3e98d1cd
Alexey <alikdolg@gmail.com>
2024-03-24 13:28:12 +0200

    Support Certificate type

--- certificates.go
+++ certificates.go
@@ -3,6 +3,6 @@ package kmip
 type Certificate struct {
 	Tag `kmip:"CERTIFICATE"`
 
-	CertificateType  CertificateType `kmip:"CERTIFICATE_TYPE,required"`
-	CertificateValue []byte          `kmip:"CERTIFICATE_VALUE,required"`
+	CertificateType  Enum   `kmip:"CERTIFICATE_TYPE,required"`
+	CertificateValue []byte `kmip:"CERTIFICATE_VALUE,required"`
 }
